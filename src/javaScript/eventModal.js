878c1219e49af307ae58c46e41ff31ce5656e04c
jo4o252000 <joaovictor252000@gmail.com>
2023-03-18 18:54:48 -0300

    first commit

---
+++ src/javaScript/eventModal.js
@@ -0,0 +1,10 @@
+const dialog = document.getElementById('stackDialog')
+const showButton = document.getElementById('grid1')
+
+const teste1 = function showDialog(valueID){  
+    dialog.showModal()
+      
+}
+
+
+exports = teste1
